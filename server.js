98cda23deff81b31c7723c1b1f6fd1f2d9f19261
Fliplope <fliplope@gmail.com>
2017-03-21 17:24:49 +0900

    test success

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var fs = require("fs")
 var port = process.env.PORT || 8080;
-
 var server = app.listen(port, function(){
-    console.log("Express server has started on port 3000")
+    console.log("Express server has started on port 8080")
 });
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(session({
-    secret: '@#@$MYSIGN#@$#$',
     resave: false,
     saveUninitialized: true
 }));
