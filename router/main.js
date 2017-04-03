82460cbc13d7e77ae7298923829a57b1ec5c07fc
Slopcat <slopcat99@gmail.com>
2017-04-03 16:06:27 +0900

    Korea to English update

--- router/main.js
+++ router/main.js
@@ -1,146 +1,154 @@
 // 카카오톡 모듈
-module.exports = function (app, fs) {
-	// 키보드
-	app.get('/keyboard', function (req, res) {
-		fs.readFile(__dirname + "/../data/" + "keyboard.json", 'utf8', function (err, data) {
-			console.log(data);
-			res.end(data);
-		});
-	});
+module.exports = function(app, fs) {
+    // 키보드
+    app.get('/keyboard', function(req, res) {
+        fs.readFile(__dirname + "/../data/" + "keyboard.json", 'utf8', function(err, data) {
+            console.log(data);
+            res.end(data);
+        });
+    });
 
-	// 메시지
-	app.post('/message', function (req, res) {
-		var result = {};
+    // 메시지
+    app.post('/message', function(req, res) {
+        var result = {};
 
-		// CHECK REQ VALIDITY
-		if (!req.body["user_key"] || !req.body["type"] || !req.body["content"]) {
-			result["success"] = 0;
-			result["error"] = "invalid request";
-			res.json(result);
-			return;
-		}
+        // CHECK REQ VALIDITY
+        if (!req.body["user_key"] || !req.body["type"] || !req.body["content"]) {
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
 
-		if (req.body["content"] == "도움말" || req.body["content"] == "만든이") {
-			fs.readFile(__dirname + "/../data/message.json", 'utf8', function (err, data) {
-				var messages = JSON.parse(data);
+        if (req.body["content"] == "도움말" || req.body["content"] == "만든이") {
+            fs.readFile(__dirname + "/../data/message.json", 'utf8', function(err, data) {
+                var messages = JSON.parse(data);
+                if (req.body["content"] == "도움말") {
+                    messages["message"] = {
+                        "text": "영어단어를 입력하시면 뜻이 표시됩니다.\n반대로 한글을 입력하면 영어단어가 표시됩니다."
+                    };
+                } else {
+                    messages["message"] = {
+                        "text": "slopcat99.dothome.co.kr에서 개발하였습니다."
+                    };
+                }
 
-				if (req.body["content"] == "도움말")
-					messages["message"] = {
-						"text": "영어단어를 입력하시면 뜻이 표시됩니다.\n반대로 한글을 입력하면 영어단어가 표시됩니다."
-					};
-				else
-					messages["message"] = {
-						"text": "Fliplope가 개발하였습니다."
-					};
+                fs.writeFile(__dirname + "/../data/message.json", JSON.stringify(messages, null, '\t'), "utf8", function(err, data) {})
+                fs.readFile(__dirname + "/../data/message.json", 'utf8', function(err, data) {
+                    console.log("Request_user_key : " + req.body["user_key"]);
+                    console.log("Request_type : keyboard - " + req.body["content"]);
+                    res.end(data);
+                    return;
+                })
+            })
+        } else {
+            // 단어 파싱
+            var request = require('request');
+            var cheerio = require("cheerio");
+            var han = /[ㄱ-ㅎ|ㅏ-ㅣ|가-?]/g;
+            var chk_han = req.body["content"].match(han);
+            var url = "";
+            if (!chk_han)
+                url = 'http://alldic.daum.net/search.do?q=' + req.body["content"];
+            else
+                url = 'http://dic.daum.net/word/view.do?wordid=kew000097072&q=' + req.body["content"];
 
-				fs.writeFile(__dirname + "/../data/message.json", JSON.stringify(messages, null, '\t'), "utf8", function (err, data) {})
+            fs.readFile(__dirname + "/../data/message.json", 'utf8', function(err, data) {
+                var messages = JSON.parse(data);
+                request(url, function(error, response, body) {
+                    var $ = cheerio.load(body);
+                    var wordpage = $("#mArticle div.cleanword_type.kuke_type").first();
+                    var word = wordpage.find("div.search_cleanword strong a span").text();
+                    var meanUl = $("ul.list_search").first();
+                    var meanLi = $(meanUl).find("li");
+                    var meanArray = "";
+                    meanLi.each(function() {
+                        var num = $(this).find("li > span.num_search").text();
+                        var en_mean = $(this).find("li > span.txt_search").text();
+                        meanArray += num + en_mean + " ";
+                    });
 
-				fs.readFile(__dirname + "/../data/message.json", 'utf8', function (err, data) {
-					console.log("Request_user_key : " + req.body["user_key"]);
-					console.log("Request_type : keyboard - " + req.body["content"]);
-					res.end(data);
-					return;
-				})
-			})
-		} else {
-			// 단어 파싱
-			var request = require('request');
-			var cheerio = require("cheerio");
-			var url = 'http://alldic.daum.net/search.do?q=' + req.body["content"];
+                    messages["message"] = {
+                        "text": word + "\n" + meanArray
+                    };
 
-			fs.readFile(__dirname + "/../data/message.json", 'utf8', function (err, data) {
-				var messages = JSON.parse(data);
-				request(url, function (error, response, body) {
-					var $ = cheerio.load(body);
-					var wordpage = $("#mArticle div.cleanword_type.kuek_type").first();
-					var word = wordpage.find("div.search_cleanword strong a").text();
-					var means = $("ul.list_search").first();
-					var meaning = $(means).find("li").text();
+                    fs.writeFile(__dirname + "/../data/message.json", JSON.stringify(messages, null, '\t'), "utf8", function(err, data) {})
+                    fs.readFile(__dirname + "/../data/message.json", 'utf8', function(err, data) {
+                        res.end(data);
+                        return;
+                    })
+                });
+            })
+        }
+    });
 
-					messages["message"] = {
-						"text": word.toString() + "\n" + meaning.toString()
-					};
+    // 친구추가
+    app.post('/friend', function(req, res) {
+        var result = {};
 
-					fs.writeFile(__dirname + "/../data/message.json", JSON.stringify(messages, null, '\t'), "utf8", function (err, data) {})
+        // 요청 param 체크
+        if (!req.body["user_key"]) {
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
 
-					fs.readFile(__dirname + "/../data/message.json", 'utf8', function (err, data) {
-						console.log("Request_user_key : " + req.body["user_key"]);
-						console.log("Request_type : keyboard - " + req.body["content"]);
-						res.end(data);
-						return;
-					})
-				});
-			})
-		}
-	});
+        // 파일 입출력
+        fs.readFile(__dirname + "/../data/friend.json", 'utf8', function(err, data) {
+            var users = JSON.parse(data);
+            // 이미 존재하는 친구일 경우
+            if (users[req.body["user_key"]]) {
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
 
-	// 친구추가
-	app.post('/friend', function (req, res) {
-		var result = {};
+            // 친구추가
+            users[req.body["user_key"]] = req.body;
+            fs.writeFile(__dirname + "/../data/friend.json",
+                JSON.stringify(users, null, '\t'), "utf8",
+                function(err, data) {
+                    result = 200;
+                    res.json(result);
+                    return;
+                })
+        })
+    });
 
-		// 요청 param 체크
-		if (!req.body["user_key"]) {
-			result["success"] = 0;
-			result["error"] = "invalid request";
-			res.json(result);
-			return;
-		}
+    // 친구삭제(차단)
+    app.delete('/friend/:user_key', function(req, res) {
+        var result = {};
 
-		// 파일 입출력
-		fs.readFile(__dirname + "/../data/friend.json", 'utf8', function (err, data) {
-			var users = JSON.parse(data);
-			// 이미 존재하는 친구일 경우
-			if (users[req.body["user_key"]]) {
-				result["success"] = 0;
-				result["error"] = "duplicate";
-				res.json(result);
-				return;
-			}
+        // 파일 입출력
+        fs.readFile(__dirname + "/../data/friend.json", "utf8", function(err, data) {
+            var users = JSON.parse(data);
 
-			// 친구추가
-			users[req.body["user_key"]] = req.body;
-			fs.writeFile(__dirname + "/../data/friend.json",
-				JSON.stringify(users, null, '\t'), "utf8",
-				function (err, data) {
-					result = 200;
-					res.json(result);
-					return;
-				})
-		})
-	});
+            // 존재하지 않는 친구일 경우
+            if (!users[req.params.user_key]) {
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+            // 친구 삭제
+            delete users[req.params.user_key];
+            fs.writeFile(__dirname + "/../data/friend.json",
+                JSON.stringify(users, null, '\t'), "utf8",
+                function(err, data) {
+                    result = 200;
+                    res.json(result);
+                    return;
+                })
+        })
+    })
 
-	// 친구삭제(차단)
-	app.delete('/friend/:user_key', function (req, res) {
-		var result = {};
-
-		// 파일 입출력
-		fs.readFile(__dirname + "/../data/friend.json", "utf8", function (err, data) {
-			var users = JSON.parse(data);
-
-			// 존재하지 않는 친구일 경우
-			if (!users[req.params.user_key]) {
-				result["success"] = 0;
-				result["error"] = "not found";
-				res.json(result);
-				return;
-			}
-			// 친구 삭제
-			delete users[req.params.user_key];
-			fs.writeFile(__dirname + "/../data/friend.json",
-				JSON.stringify(users, null, '\t'), "utf8",
-				function (err, data) {
-					result = 200;
-					res.json(result);
-					return;
-				})
-		})
-	})
-
-	// 채팅방 나가기
-	app.delete('/chat_room/:user_key', function (req, res) {
-		var result = {};
-		result = 200;
-		res.json(result);
-		return;
-	})
+    // 채팅방 나가기
+    app.delete('/chat_room/:user_key', function(req, res) {
+        var result = {};
+        result = 200;
+        res.json(result);
+        return;
+    })
 }
